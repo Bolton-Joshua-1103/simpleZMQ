c1d089601748383ce6f3a85a4dd364aa4dd97a4b
Bolton-Joshua-1103 <jabolton33@gmail.com>
2026-09-05 11:48:34 -0700

    Simple change to some CMake build paths to leave room for windows compile in the future

--- scripts/buildAll.sh
+++ scripts/buildAll.sh
@@ -2,8 +2,9 @@
 
 ORIGINAL_DIR="$(pwd)"
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+DEFAULT_PRESET="WSL-default"
 REPO_DIR="$SCRIPT_DIR/.." #scripts directory is assumed to be always at <REPO_ROOT>/scripts
-BUILD_DIR="$REPO_DIR/build"
+BUILD_DIR="$REPO_DIR/build/$DEFAULT_PRESET"
 SUBMODULE_DIR="$REPO_DIR/Submodules"
 
 #VCPKG REQUIRED PACKAGES
@@ -25,10 +26,14 @@ git submodule update --init --recursive
 $SUBMODULE_DIR/vcpkg/bootstrap-vcpkg.sh
 
 #CMake build
+echo "==== VCPKG SETUP COMPLETE ===="
 mkdir -p $BUILD_DIR
+echo "==== BUILD DIR: $BUILD_DIR"
 cd $BUILD_DIR
-cmake .. --preset WSL-default
+echo "==== BEGIN CMAKE BUILD ===="
+cmake ../.. --preset $DEFAULT_PRESET
 cmake --build .
+echo "==== CMAKE BUILD COMPLETE ===="
 
 cleanup() {
     echo "====== buildAll.sh Complete ======"
